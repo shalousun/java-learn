a3d7b810ebb6aacbe0c2324c2547f6467f807338
oppofind <836575280@qq.com>
2020-11-09 00:06:22 +0800

    添加netty

--- custom-rpc/src/main/java/com/yusun/rpc/protocal/dubbo/NettyServerHandler.java
+++ custom-rpc/src/main/java/com/yusun/rpc/protocal/dubbo/NettyServerHandler.java
@@ -1,7 +1,9 @@
 package com.yusun.rpc.protocal.dubbo;
 
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
 /**
  * @author yu 2020/11/8.
  */
-public class NettyServerHandler {
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
 }
